b977b2cadf16c9c6473db936098c408340cc3172
Lucía Pereyra <84113404+luciapaulinadiaz@users.noreply.github.com>
2025-08-15 20:45:46 -0300

    Initial commit

---
+++ src/main/java/com/starwars/app/StarwarsAppApplication.java
@@ -0,0 +1,13 @@
+package com.starwars.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starwars/app/StarwarsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starwars.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
